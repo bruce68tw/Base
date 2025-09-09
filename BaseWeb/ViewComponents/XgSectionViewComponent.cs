8948e606e24fefcac2598386eff22849a2b5d230
Bruce Chen <bruce68tw@gmail.com>
2025-09-09 23:28:19 +0800

    fix drag

---
+++ BaseWeb/ViewComponents/XgSectionViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaseWeb.ViewComponents
+{
+    public class XgSectionViewComponent : ViewComponent
+    {
+        public HtmlString Invoke(string label)
+        {
+            var html = $"<div class='x-section'>{label}</div>";
+            return new HtmlString(html);
+        }
+
+    } //class
+}
