7c9493879dc5fc3d3402114d0cb4a60723a69176
Bruce Chen <bruce68tw@gmail.com>
2025-11-06 00:46:57 +0800

    crud to genCrud

--- Base/Services/_Var.cs
+++ Base/Services/_Var.cs
@@ -1,4 +1,6 @@
-﻿namespace Base.Services
+﻿using DocumentFormat.OpenXml.Office2013.PowerPoint.Roaming;
+
+namespace Base.Services
 {
     public class _Var
     {
@@ -11,5 +13,14 @@
         {
             return !_Var.IsEmpty(data);
         }
+
+        public static bool ToBool(object? data)
+        {
+            if (data == null) return false;
+            var value = data.ToString()!;
+            return (value == "1" || value.ToLower() == "true");
+                ;
+        }
+
     }
 }
