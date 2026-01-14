c9e06cd177e83352fe08e882f4c4aef0be9191d3
bruce <bruce68tw@gmail.com>
2026-01-14 12:34:16 +0800

    SubsidyItem 固定顯示4筆

---
+++ Base/Services/_Code.cs
@@ -0,0 +1,38 @@
+﻿using Base.Models;
+using Newtonsoft.Json.Linq;
+using SkiaSharp;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Base.Services
+{
+    /// <summary>
+    /// 處理 XpCode table
+    /// </summary>
+    public class _Code
+    {
+        //filter json array
+        public static JArray? FilterArray(JArray rows, string fid, string value)
+        {
+            //if (rows == null) return null;
+
+            var finds = new JArray();
+            foreach (var row in rows)
+                if (row[fid]!.ToString() == value) finds.Add(row);
+            return (finds.Count == 0)
+                ? null : finds;
+        }
+
+        public static List<IdStrDto>? FilterRows(List<IdStrExtDto>? rows, string value)
+        {
+            return (rows == null || rows.Count == 0)
+                ? null
+                : rows.Where(a => a.Ext == value)
+                    .Select(a => new IdStrDto { Id = a.Id, Str = a.Str })
+                    .ToList();
+        }
+
+        /// <summary>
+    }//class
+}
