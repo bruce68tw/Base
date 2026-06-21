55fc186f1a0e381ee3f9cbb56c27ff949ee88fe4
Bruce Chen <bruce68tw@gmail.com>
2026-06-21 11:44:50 +0800

    add comment

--- Base/Models/EditDto.cs
+++ Base/Models/EditDto.cs
@@ -5,109 +5,94 @@ using System.Collections.Generic;
 namespace Base.Models
 {
     /// <summary>
-    /// for crud edit form
+    /// crud edit form model
     /// </summary>
     public class EditDto
     {
         /// <summary>
-        /// (1st)transaction or not, for master table only
-        /// true: 由系統自行動控制
+        /// (用於Master table) 是否由系統自動設定 transaction, 預設為true, 如果要自行控制, 則設為false
         /// </summary>
         public bool AutoTrans = true;
 
         /// <summary>
-        /// (1st)has foreign or not, 如果為true, 則由DB自行刪除關聯資料
-        /// </summary>
-        //public bool HasFKey = true;
-
-        /// <summary>
-        /// 是否可以新增key值
-        /// </summary>
-        //public bool CanAddKey = false;
-
-        /// <summary>
-        /// auto Id length, empty for default.<br/>
-        /// values: 0表示允許前端傳入新Id、_Fun.AutoIdShort(6)、_Fun.AutoIdMid(10)、_Fun.AutoIdLong(16)
+        /// 自動產生主key的字串長度, 預設為中等長度 10 個字元, 全部清單有: 6、10、16<br/>
+        /// 如果設為0則表示允許前端傳入新Id
         /// </summary>
         public int AutoIdLen = _Fun.AutoIdMid;
 
         /// <summary>
-        /// table name
+        /// 要存取的 table name
         /// </summary>
         public string Table = "";
 
         /// <summary>
-        /// sql string, if not empty, will read db<br/>
-        /// master/child-1 use "=", ex: Id=@Id<br/>
-        /// child-2 use "in", ex: Id in ({0}), or Id in ({{0}})(has @ sign), 若是上層是單筆也可用={0}較簡單<br/>
-        /// 如果只填ReadSql, 需要同時填PkeyFid
+        /// 讀取資料庫的 sql, 如果空白則表示使用 Items 欄位來組成 sql<br/>
+        /// master table 和第一層 child table 此欄位最後面要加上 Id=@Id 來讀取該筆/多筆資料<br/>
+        /// 第二層 child table 必須使用 in , ex: Id in ({0}), or Id in ({{0}})(有 $ 符號時), 若是上層是單筆也可用={0}較簡單<br/>
+        /// 如果只填ReadSql 沒有 Items, 則需要同時設定 PkeyFid 欄位
         /// </summary>
         public string ReadSql = "";
 
         /// <summary>
         /// primary key field id
-        /// note: single pkey can not edit
         /// </summary>
         public string PkeyFid = "";
 
         /// <summary>
-        /// (2nd)foreign key field id, 空白表示不必設定, 一般用在child是1對1, 其主key與master相同 !!
+        /// (用於 child table) foreign key field id, 空白表示不必設定, 一般用在child是1對1, 其主key與master相同 !!
         /// 這個欄位不可設為 required !!
         /// </summary>
         public string FkeyFid = "";
 
         /// <summary>
-        /// field list
+        /// 要存取的欄位清單
         /// </summary>
         public EitemDto[] Items = null!;
 
         /// <summary>
-        /// field list for empty to null, usually for data field
-        /// 空的日期欄位存入DB會變成1900/1/1, 必須先轉成null !!
+        /// 有些欄位如果傳入空白, 則要先轉成 NULL 再存入資料庫，則將欄位Id填入此欄位
+        /// 例如: 空的日期欄位存入DB會變成1900/1/1, 必須先轉成null !!
         /// </summary>
         public string[] EmptyToNulls = [];
 
         /// <summary>
-        /// Col4可為null, 元素也可為null
-        /// creator, created datetime, reviser, revised datetime
-        /// consider time difference
-        /// default has 4 fields, set this to null if none
+        /// 表示4個常見的欄位，依序為: Creator、Created、Reviser、Revised
+        /// 預設有這4個欄位, 如果無則設為null
+        /// 時間欄位系統會考慮登入者的時差問題
         /// </summary>
         public string?[]? Col4 = ["Creator", "Created", "Reviser", "Revised"];
 
         /// <summary>
-        /// (2nd)order by string (not include "order by")
+        /// (用於 child table) 資料的排序, 此欄位本身不含 "order by" 關鍵字
         /// </summary>
         public string OrderBy = "";
 
         /// <summary>
-        /// child edit form list
+        /// child table list
         /// </summary>
-        //public List<EditDto> Childs = null;
         public EditDto[]? Childs = null;
 
         /// <summary>
-        /// fid no 對應, 儲存資料時會系統重新設定, PG不必處理
+        /// 異動的 Fid 和序號的對照表, 儲存資料時會系統重新設定, 由系統自動設定
         /// 如果無異動欄位, 則此變數為 null
         /// </summary>
         public JObject? _FidNo = null;
 
         /// <summary>
-        /// 必填欄位清單, 儲存資料時會系統重新設定, PG不必處理
+        /// 必填欄位清單, 儲存資料時會系統重新設定, 由系統自動設定
         /// </summary>
         public List<string>? _FidRequires = null;
 
         /// <summary>
-        /// 後端自定義欄位驗證
+        /// 後端自定義欄位驗證檢查函數
         /// <param name="bool">isNew fun or not</param>
-        /// <returns>validate error list if any</returns>
+        /// <returns>驗證錯誤的欄位清單, 資料型態為 List<ErrorRowDto></returns>
         /// </summary>
-        //public Func<bool, JObject, List<ErrorRowDto>?>? FnValidate = null;
         public FnValidateA? FnValidateA = null;
 
         /// <summary>
-        /// crud edit WhenSave before transaction
-        /// 參考 DbAdm GenCrudUiEdit.cs
+        /// 儲存資料之前要觸發的函數，發生在 transaction 之前
+        /// 使用範例參考 DbAdm GenCrudUiEdit.cs
         /// </summary>
         /// <param name="bool">isNew fun or not</param>
         /// <param name="CrudEditSvc"></param>
@@ -117,8 +102,8 @@ namespace Base.Models
         public FnWhenSaveA? FnWhenSaveA = null;
 
         /// <summary>
-        /// crud edit AfterSave, inside transaction, 此時已經儲存全部table, 但未commit
-        /// 參考 HrAdm LeaveEdit.cs CreateA()、BaoAdm BaoEdit.cs
+        /// 儲存資料之後要觸發的函數，此時已經啟動 transaction, 並且完成儲存全部table, 但未commit
+        /// 使用範例參考 HrAdm LeaveEdit.cs CreateA()、BaoAdm BaoEdit.cs
         /// </summary>
         /// <param name="bool">true(新增), false(修改)</param>
         /// <param name="CrudEditSvc"></param>
@@ -128,20 +113,19 @@ namespace Base.Models
         public FnAfterSaveA? FnAfterSaveA = null;
 
         /// <summary>
-        /// 自行函數 for 設定 new key
+        /// 自行函數用來傳回 new key
         /// </summary>
         public FnGetNewKeyA? FnGetNewKeyA = null;
 
         /// <summary>
         /// set new keyJson
         /// </summary>
-        /// <param name="isNew">isNew fun or not</param>
+        /// <param name="isNew">bool, isNew fun or not</param>
         /// <param name="crudEditSvc">CrudEdit service</param>
         /// <param name="inputJson">input json</param>
-        /// <param name="editDto">edit dto</param>
+        /// <param name="editDto">EditDto</param>
         /// <returns>error msg if any</returns>
         public FnSetNewKeyJsonA? FnSetNewKeyJsonA = null;
-        //(bool isNew, CrudEditSvc crudEditSvc, JObject inputJson, EditDto editDto);
 
     }//class
 }
--- Base/Models/EitemDto.cs
+++ Base/Models/EitemDto.cs
@@ -3,44 +3,60 @@
 namespace Base.Models
 {
     /// <summary>
-    /// for crud edit form field
+    /// 用來設定一個CRUD編輯欄位
     /// </summary>
     public class EitemDto
     {
-        //frent side field id
+        /// <summary>
+        /// 前端UI的欄位Id
+        /// </summary>
         public string Fid = "";
 
-        //where column id, has table alias
+        /// <summary>
+        /// 要存取的資料表欄位名稱，包含 table alias，如果空白表示同 Fid 欄位
+        /// </summary>
         public string Col = "";
 
-        //field type
-        //public QitemTypeEnum Type = QitemTypeEnum.None;
-
-        //check type
+        /// <summary>
+        /// 資料驗證方式，參考 CheckTypeEstr.cs
+        /// </summary>
         public string CheckType = CheckTypeEstr.None;
 
         //check data for CheckType, ex:
-        //"1,5" for Range, "1" for Min/Max
+        //
+        /// <summary>
+        /// 資料驗證的資料內容，配合 CheckType 欄位，例如: CheckType="Min", CheckData="1"，則表示最小值必須大於等於1
+        /// </summary>
         public string CheckData = "";
 
         /// <summary>
-        /// default value when add new
+        /// 新增一筆資料時這個Fid欄位的預設值
         /// </summary>
         public object? Value;
 
-        //required or not
+        /// <summary>
+        /// required or not
+        /// </summary>
         public bool Required = false;
 
-        //if true, Create/Update will skip
+        /// <summary>
+        /// if true, Create/Update will skip
+        /// </summary>
         public bool Read = false;
 
-        //allow create 
+        /// <summary>
+        /// Fid欄位是否允許新增
+        /// </summary>
         public bool Create = true;
 
-        //allow update
+        /// <summary>
+        /// Fid欄位是否允許修改
+        /// </summary>
         public bool Update = true;
 
-        //allow update
+        /// <summary>
+        /// Fid欄位是否為HTML, 如果是則系統會自動編碼/解碼
+        /// </summary>
         public bool IsHtml = false;
     }
 }
