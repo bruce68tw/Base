ed641ff10d2c6fec5acb73bc13cf9409e9e2e610
bruce <bruce68tw@gmail.com>
2026-08-25 11:38:24 +0800

    add LLM function

---
+++ Base/Models/LlmAskDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Base.Models
+{
+    //LLM 問題
+    public class LlmAskDto
+    {
+        //system role
+        public string System { get; set; } = "";
+        //user role
+        public string User { get; set; } = "";
+    }
+}
